d35df1e738e276e58dfe3aa0c9b93c1cc412b989
Ragini340 <ragini31bxr@gmail.com>
2022-12-04 21:33:21 +0530

    Add project files.

---
+++ StartPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCMock
+{
+    public class StartPage
+    {
+
+        virtual public string Name {
+            get; set; 
+        }
+    }
+}
